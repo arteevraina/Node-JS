b4613b1f5ae1704e5ec7adae8074e0e144752164
Arteev Raina <arteevraina@gmail.com>
2020-05-26 03:04:30 +0530

    Mongoose Population!

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-var User = new Schema({
+var User = new Schema({ 
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
